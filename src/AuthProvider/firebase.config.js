9cc0c9236eb35cb8061428a9dad3ab201c5effa4
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2022-11-15 01:39:09 +0600

    new firebase add

--- src/AuthProvider/firebase.config.js
+++ src/AuthProvider/firebase.config.js
@@ -1,11 +1,11 @@
 // Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// const dotenv =require('dotenv')
-// const env = dotenv.config().parsed;
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOAMIN,
@@ -13,7 +13,9 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
-// console.log(firebaseConfig);
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
